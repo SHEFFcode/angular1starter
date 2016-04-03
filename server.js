023ade37005520ce20d605843e290678c4f9ec3d
Yuriy Shefer <sheff@sheffmachine.com>
2016-04-02 17:51:39 -0700

    keep playing around with auth

--- server.js
+++ server.js
@@ -131,15 +131,9 @@ passport.use(new GoogleStrategy({
   },
 
   function(accessToken, refreshToken, profile, done) {
-
-    if(profile){
       Runner.findOrCreate({ runner_id: profile.id }, {first_name: profile.displayName, email: profile.emails[0].value}, function (err, user) {
         return done(err, user);
       });
-
-    } else {
-      return done(null, false); 
-    }
   }
 
 ));
